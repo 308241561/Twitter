56080a987d488dcedfd9841bef15dbd9214da8ee
308241561 <huangxingguo1986@gmail.com>
2022-03-13 23:27:17 -0700

    end of the required stories

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -19,6 +19,7 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var favButton: UIButton!
     var favorited:Bool = false
     var tweetId:Int = -1
+
     
     @IBAction func favoriteTweet(_ sender: Any) {
         let toBeFavoried = !favorited
@@ -40,8 +41,22 @@ class TweetCellTableViewCell: UITableViewCell {
     
     
     @IBAction func retweet(_ sender: Any) {
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (error) in
+            print("Error in retweet: \(error)")
+        })
     }
     
+    func setRetweeted(_ isRetweeted:Bool){
+        if (isRetweeted){
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        }else{
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
+    }
     
 
     
